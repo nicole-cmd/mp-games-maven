60cc965c9a2993b04d50cd3240ff8465bef281d2
Cade <johnston2@grinnell.edu>
2024-11-06 16:26:29 -0600

    Added Docstrings

--- src/main/java/edu/grinnell/csc207/util/Tile.java
+++ src/main/java/edu/grinnell/csc207/util/Tile.java
@@ -1,14 +1,21 @@
 package edu.grinnell.csc207.util;
 
+/**
+ * An object that represents a tile in Sayu.
+ * Includes methods to flip, rotate, and get qualities
+ *
+ * @author Cade Johnston
+ * @author Nicole Gorrell
+ */
 public class Tile {
   // +--------+------------------------------------------------------
   // | Fields |
   // +--------+
 
-  /** The direction of the arrow in the center of the piece. */
+  /** The direction of the arrow in the center (white arrow) of the piece. */
   private int baseDir;
 
-  /** The direction of the arrows on the edges of the piece. */
+  /** The direction of the arrows on the edges (color arrows) of the piece. */
   private int[] outerDir;
 
   /** Who has contol of the current piece. P1=false, P2=true. */
@@ -24,6 +31,14 @@ public class Tile {
   // | Constructors |
   // +--------------+
 
+  /**
+   * Create a Tile object.
+   * 
+   * @param base
+   *   The direction of the white arrow of the tile.
+   * @param outer
+   *   The directions of the color arrows of the tile.
+   */
    public Tile(int base, int[] outer) {
     baseDir = base;
     outerDir = outer;
@@ -62,6 +77,12 @@ public class Tile {
     return false;
   } // canFlip(Tile, int, int, int, int)
 
+  /**
+   * Rotate this Sayu tile.
+   * 
+   * @param amount
+   *   The direction to set the white arrow of this tile to.
+   */
   public void rotate(int amount) {
     if (this.baseDir != -1) {
       for (int i = 0; i < this.outerDir.length; i++) {
@@ -71,12 +92,41 @@ public class Tile {
     } // if
   } // rotate(int)
 
+  /**
+   * Return the owner of this Sayu tile.
+   * 
+   * @return
+   *   The owner of this Sayu tile.
+   */
   public boolean getOwner() {
     return this.owner;
   } // getOwner()
 
+  /**
+   * Change the owner of this Sayu tile.
+   */
   public void flip() {
     this.owner = !(this.owner);
   } // flip()
+
+  /**
+   * Gets an array representing the white arrow of this Sayu tile.
+   * 
+   * @return
+   *   An int representing the white arrow of this Sayu tile.
+   */
+  public int getDir() {
+    return this.baseDir;
+  } // getDir()
+
+  /**
+   * Gets an array representing the color arrows of this Sayu tile.
+   * 
+   * @return
+   *   An int array representing the color arrows of this Sayu tile.
+   */
+  public int[] getOuterDirs() {
+    return this.outerDir;
+  } // getOuterDirs()
 
 } // Tile
