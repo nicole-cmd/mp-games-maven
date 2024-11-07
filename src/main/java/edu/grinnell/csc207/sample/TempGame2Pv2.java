daeb6cbb6f4f68308d3df3b903bc56d93ba4a0bd
Cade <johnston2@grinnell.edu>
2024-11-07 13:13:28 -0600

    Updated Visual Output/Interface

--- src/main/java/edu/grinnell/csc207/sample/TempGame2Pv2.java
+++ src/main/java/edu/grinnell/csc207/sample/TempGame2Pv2.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.event.*;
 import edu.grinnell.csc207.util.*;
 import java.util.Random;
+import java.awt.Color;
 
 public class TempGame2Pv2 {
   public static void main(String[] args) {
@@ -11,7 +12,7 @@ public class TempGame2Pv2 {
     JTextField x=new JTextField();
     x.setBounds(50,100,40,20);
     JButton b=new JButton("Create with above seed");
-    JButton[] fieldGrid = new JButton[52];
+    JButton[] fieldGrid = new JButton[51];
     b.setBounds(50,200,95,30);
     b.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
@@ -23,35 +24,27 @@ public class TempGame2Pv2 {
         } // if / else
         b.setVisible(false);
         x.setVisible(false);
-        for(int i = 0; i < 49; i++){
+        for(int i = 0; i < 51; i++){
           fieldGrid[i] = new JButton();
           fieldGrid[i].setName(i+"");
-          fieldGrid[i].addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-              System.out.println("Event occured in button: " + ((JButton)e.getSource()).getName());
-            } // actionPerformed(ActionEvent)
-          }); // addActionListener(ActionListener)
+          fieldGrid[i].setRolloverEnabled(false);
+          if (i < 49) {
+            fieldGrid[i].setBackground(new Color(0, 0, 0, 0));
+            fieldGrid[i].setBorder(null);
+            fieldGrid[i].addActionListener(new ActionListener() {
+              public void actionPerformed(ActionEvent e){
+                ((JButton)e.getSource()).setVisible(false);
+                System.out.println("Event occured in button: " + ((JButton)e.getSource()).getName());
+              } // actionPerformed(ActionEvent)
+            }); // addActionListener(ActionListener)
+          } // if
           f.add(fieldGrid[i]);
           fieldGrid[i].setBounds(25 + (i % 7) * 35, 80 + (i / 7) * 35, 35, 35);
         } // for [i]
-        fieldGrid[50] = new JButton();
-        fieldGrid[50].setName("Rotate Clockwise");
-        fieldGrid[50].addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent e){
-            System.out.println("Event occured in button: " + ((JButton)e.getSource()).getName());
-          } // actionPerformed(ActionEvent)
-        }); // addActionListener(ActionListener)
-        f.add(fieldGrid[50]);
-        fieldGrid[50].setBounds(300, 175, 25, 55);
-        fieldGrid[51] = new JButton();
-        fieldGrid[51].setName("Rotate Counterclockwise");
-        fieldGrid[51].addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent e){
-            System.out.println("Event occured in button: " + ((JButton)e.getSource()).getName());
-          } // actionPerformed(ActionEvent)
-        }); // addActionListener(ActionListener)
-        f.add(fieldGrid[51]);
-        fieldGrid[51].setBounds(375, 175, 25, 55);
+        fieldGrid[49].setName("Rotate Clockwise");
+        fieldGrid[50].setName("Rotate Counterclockwise");
+        fieldGrid[49].setBounds(300, 175, 25, 55);
+        fieldGrid[50].setBounds(375, 175, 25, 55);
       } // actionPerformed(ActionEvent)
     }); // addActionListener(ActionListener)
     f.add(b);
