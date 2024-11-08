aa21a5fe53f9530be2d933edc3219c704a94fb8f
NICOLE_GORRELL <GORRELLN@grinnell.edu>
2024-11-08 16:30:19 -0600

    Style checking Tile.java

--- src/main/java/edu/grinnell/csc207/util/Tile.java
+++ src/main/java/edu/grinnell/csc207/util/Tile.java
@@ -2,7 +2,7 @@ package edu.grinnell.csc207.util;
 
 /**
  * An object that represents a tile in Sayu.
- * Includes methods to flip, rotate, and get qualities
+ * Includes methods to flip, rotate, and get qualities.
  *
  * @author Cade Johnston
  * @author Nicole Gorrell
@@ -12,20 +12,20 @@ public class Tile {
   // | Fields |
   // +--------+
 
-  /** The direction of the arrow in the center (white arrow) of the piece. */
+  /** The direction of the white arrow in the center of the piece. */
   private int baseDir;
 
-  /** The direction of the arrows on the edges (color arrows) of the piece. */
+  /** The direction of the color arrows on the edges of the piece. */
   private int[] outerDir;
 
-  /** Who has contol of the current piece. P1=false, P2=true. */
+  /** Who has contol of the current piece: P1=false, P2=true. */
   private boolean owner;
 
-  /** The change in x expected with direction 'index' */
-  private static final int[] DELTASX = new int[]{0,1,1,1,0,-1,-1,-1};
+  /** The change in x expected with direction 'index'. */
+  private static final int[] DELTASX = new int[]{0, 1, 1, 1, 0, -1, -1, -1};
 
-  /** The change in y expected with direction 'index' */
-  private static final int[] DELTASY = new int[]{1,1,0,-1,-1,-1,0,1};
+  /** The change in y expected with direction 'index'. */
+  private static final int[] DELTASY = new int[]{1, 1, 0, -1, -1, -1, 0, 1};
 
   // +--------------+------------------------------------------------
   // | Constructors |
@@ -35,11 +35,11 @@ public class Tile {
    * Create a Tile object.
    * 
    * @param base
-   *   The direction of the white arrow of the tile.
+   *  The direction of the white arrow of the tile.
    * @param outer
-   *   The directions of the color arrows of the tile.
+   *  The directions of the color arrows of the tile.
    */
-   public Tile(int base, int[] outer) {
+  public Tile(int base, int[] outer) {
     this.baseDir = base;
     this.outerDir = outer;
     this.owner = false;
@@ -49,6 +49,21 @@ public class Tile {
   // | Methods |
   // +---------+
 
+  /**
+   * Determines if we can flip the tile adjacent to that we placed.
+   * 
+   * @param other
+   *  The tile we want to flip.
+   * @param thisX
+   *  The x-coordinate of the tile we placed.
+   * @param thisY
+   *  The y-coordinate of the tile we placed.
+   * @param otherX
+   *  The x-coordinate of the tile we want to flip.
+   * @param otherY
+   *  The y-coordinate of the tile we want to flip.
+   * @return a boolean to determine whether we can flip the desired tile.
+   */
   public boolean canFlip(Tile other, int thisX, int thisY, int otherX, int otherY) {
     if (other == null){
       return false;  
@@ -81,7 +96,7 @@ public class Tile {
    * Rotate this Sayu tile.
    * 
    * @param amount
-   *   The direction to set the white arrow of this tile to.
+   *  The direction to set the white arrow of this tile to.
    */
   public void rotate(int amount) {
     if (this.baseDir != -1) {
@@ -95,8 +110,7 @@ public class Tile {
   /**
    * Return the owner of this Sayu tile.
    * 
-   * @return
-   *   The owner of this Sayu tile.
+   * @return the owner of this Sayu tile
    */
   public boolean getOwner() {
     return this.owner;
@@ -113,7 +127,7 @@ public class Tile {
    * Gets an array representing the white arrow of this Sayu tile.
    * 
    * @return
-   *   An int representing the white arrow of this Sayu tile.
+   *  An int representing the white arrow of this Sayu tile.
    */
   public int getDir() {
     return this.baseDir;
@@ -121,9 +135,8 @@ public class Tile {
 
   /**
    * Gets an array representing the color arrows of this Sayu tile.
-   * 
    * @return
-   *   An int array representing the color arrows of this Sayu tile.
+   *  An int array representing the color arrows of this Sayu tile.
    */
   public int[] getOuterDirs() {
     return this.outerDir;
